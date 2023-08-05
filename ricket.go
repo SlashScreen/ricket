a3dbaf0d3e1dd1360557ad2043fefd9ab0a98dea
Skyrbunny <tallesttower1@gmail.com>
2023-08-05 14:47:06 -0700

    compiled to plan 9

--- ricket.go
+++ ricket.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	// Check for arguments
 	if len(os.Args) < 2 {
-		log.Panicf("No path to WASM file.")
+		log.Panicf("No path to WASM file given. Usage: ricket [path]")
 	}
 
 	ctx := context.Background()
